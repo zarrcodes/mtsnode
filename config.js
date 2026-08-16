b3a144f6324368ece6359deb97d0feee043b76b3
zarrcodes <ibnavraham@gmail.com>
2026-08-16 18:23:46 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,10 @@
 // API_BASE: kosong = localhost (Termux langsung)
-//           isi   = URL Cloudflare Tunnel (misal: https://api.mtsmumarta.sch.id)
-const API_BASE = (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' || window.location.hostname === '0.0.0.0' || window.location.hostname.startsWith('192.168.'))
+//           isi   = URL Cloudflare Tunnel
+const API_BASE = (
+  window.location.hostname === 'localhost' ||
+  window.location.hostname === '127.0.0.1' ||
+  window.location.hostname === '0.0.0.0' ||
+  window.location.hostname.startsWith('192.168.')
+)
   ? ''
-  : 'https://effectively-loads-galleries-soma.trycloudflare.com';   // URL tunnel backend
+  : 'https://api.mtsmuhammadiyahmartapura.sch.id';
